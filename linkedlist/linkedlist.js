c40443172ae83672b46a908b324e963c790f6c90
sinha97 <vk9633698@gmail.com>
2024-06-12 22:41:20 +0530

    delete the last node

--- linkedlist/linkedlist.js
+++ linkedlist/linkedlist.js
@@ -36,20 +36,42 @@ LinkedList.prototype.addAtLast = function (data) {
 }
 
 // add node at a given node
-LinkedList.prototype.addAtGivenNode=function(prevNode,data){
-    if(!prevNode){
+LinkedList.prototype.addAtGivenNode = function (prevNode, data) {
+    if (!prevNode) {
         console.log('Prev node can not be null');
         return
     }
 
-    const newNode =new Node (data,prevNode.next)
+    const newNode = new Node(data, prevNode.next)
     prevNode.next = newNode
 }
 
 // delete the first node 
-LinkedList.prototype.deleteFirstNode = function(){
-    if(!this.head){
+LinkedList.prototype.deleteFirstNode = function () {
+    if (!this.head) {
         return
     }
-    this.head= this.head.next
+    this.head = this.head.next
+}
+
+//delete the last node
+LinkedList.prototype.deleteLastNode = function () {
+    // if head=null then it means that the there is no linkedlist
+    if (!this.head) {
+        return
+    }
+
+    // if head.next is null then just assign the head to null to delete the node 
+    if (!this.head.next) {
+        this.head = null
+        return
+    }
+
+    
+    let sLast = this.head;
+    while (sLast.next.next) { // continously check the sLast.next.next,if its gives value then just increment the sLast to sLat.next
+        sLast = sLast.next
+    }
+    sLast.next = null
+
 }
