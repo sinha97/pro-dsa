bd5486b560d86a6d0d219448ef6a0a93fd7c8825
sinha97 <vk9633698@gmail.com>
2024-06-22 18:22:37 +0530

    method explation

--- Recurssion/reverseAString.js
+++ Recurssion/reverseAString.js
@@ -18,5 +18,13 @@ function recurssiveReverseString(string) {
         return string
     }
 
-    return string[string.length - 1] + recurssiveReverseString(string.substring(0, string.length - 1))
+    // return string[string.length - 1] + recurssiveReverseString(string.substring(0, string.length - 1))
+
+    // explation of above return part
+
+    let reversePart = recurssiveReverseString(string.substring(0,string.length-1))
+    let result = string[string.length-1]+reversePart
+
+    console.log(`Reversing last char : ${string[string.length-1]} added in front of ${reversePart} to form a result: ${result}`)
+    return result
 }
