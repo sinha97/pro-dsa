435a983b0ae7c262153dd2c70b89e5a947e72c77
sinha97 <vk9633698@gmail.com>
2024-07-08 17:01:43 +0530

    min-max-island length

---
+++ GraphTheory/problems/min-max-grid-islands.js
@@ -0,0 +1,46 @@
+/*
+    write a function,minimumIsland,that takes in a grid containing Ws and Ls.
+    W represent water and l represent land.the functionshould return the size of the smallest island.
+    An island is vertically or horizontally connected region of land.
+*/
+
+function maxisIslandSize(grid){
+    //TODO:
+}
+
+function minIslandSize(grid) {
+    const rows = grid.length
+    const cols = grid[0].length
+
+    const visited = new Set()
+    let smallestSize = Infinity
+
+    function exploreSize(r, c) {
+        const pos = r + "," + c
+
+        if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === "W" || visited.has(pos)) {
+            return 0
+        }
+
+        visited.add(pos)
+        let size = 1
+
+        size += exploreSize(r + 1, c)
+        size += exploreSize(r - 1, c)
+        size += exploreSize(r, c + 1)
+        size += exploreSize(r, c - 1)
+
+        return size
+    }
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === "L" && !visited.has(r + "," + c)) {
+                const size = exploreSize(r, c)
+                if (size < smallestSize) {
+                    smallestSize = size
+                }
+            }
+        }
+    }
+}
