ad415eb32d9dd76833b1349fb024ed8a48b14e83
sinha97 <vk9633698@gmail.com>
2024-07-23 23:32:07 +0530

    max-min-path-count

---
+++ Dp/problems/min_max_path_cost.js
@@ -0,0 +1,25 @@
+function maxCountpaths(grid, row = 0, col = 0, memo = {}) {
+    
+    const pos = row + "," + col
+
+    // return the chache result
+    if (pos in memo) {
+        return memo[pos]
+    }
+
+    if (row >= grid.length || col >= grid[0].length - 1
+        // || grid[row][col] === 'X'
+    ) { //X or wall or snake
+        return -Infinity
+    }
+
+    // check if you have arrived at given location
+    if (row === grid.length - 1 && col === grid[0].length - 1) {
+        return grid[row][col]
+    }
+
+    
+
+    memo[pos] = grid[row][col] + Math.max(countpaths(grid, row + 1, col, memo) + countpaths(grid, row, col + 1, memo))
+    return memo[pos]
+}
