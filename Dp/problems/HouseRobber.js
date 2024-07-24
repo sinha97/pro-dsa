cfbf778f11e2457702154113e00c727ad4fd9814
sinha97 <vk9633698@gmail.com>
2024-07-24 10:11:14 +0530

    house robber problem

---
+++ Dp/problems/HouseRobber.js
@@ -0,0 +1,13 @@
+function houseRobber(nums, i = 0, memo = {}) {
+    if (i in memo) return memo[i]
+
+    if (i >= nums.length) return 0
+
+    const skip = houseRobber(nums, i + 1, memo)
+    const rob = nums[i] + houseRobber(nums, i + 2, memo)
+
+    memo[i] = Math.max(skip, take)
+
+    return memo[i]
+
+}
